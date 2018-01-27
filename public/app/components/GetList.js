3c9895f86ef87f07a7f68ca36f09f993a054420e
wedesignstudios <deelden@gmail.com>
2018-01-27 13:47:54 -0600

    stub out Index, GetList components

---
+++ public/app/components/GetList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class GetList extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Country List Goes Here</h3>
+      </div>
+    )
+  }
+}
+
+module.exports = GetList;
---
+++ public/app/components/Index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Route } from 'react-router-dom'
+
+const GetList = require('./GetList');
+
+class Index extends Component {
+  render() {
+    console.log(process.env);
+    return (
+      <div>
+        <h1>Top 10 Around The World</h1>
+        <Route path="/:country" component={GetList} />
+      </div>
+    )
+  }
+}
+
+module.exports = Index;
