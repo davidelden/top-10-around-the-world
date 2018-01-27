a9bdbb31eade494010162463cf099cb5fe38a820
wedesignstudios <deelden@gmail.com>
2018-01-26 22:37:24 -0600

    require dotenv, remove unused routes

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,7 +9,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
